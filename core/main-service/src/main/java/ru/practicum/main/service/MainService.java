b6af13345e40c7fcf7b12f1af8e76d4fd1952bed
forsidenis <forsidenis@yandex.ru>
2026-06-24 23:30:45 +0300

    Этап 1 (выполнен)

--- core/main-service/src/main/java/ru/practicum/main/service/MainService.java
+++ core/main-service/src/main/java/ru/practicum/main/service/MainService.java
@@ -2,10 +2,12 @@ package ru.practicum.main.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 import ru.practicum.stat.client.StatsClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Import(StatsClient.class)
 public class MainService {
     public static void main(String[] args) {
--- infra/config-server/src/main/java/ru/practicum/config/ConfigServerApp.java
+++ infra/config-server/src/main/java/ru/practicum/config/ConfigServerApp.java
@@ -1,13 +1,15 @@
-package ru.practicum.stat.server;
+package ru.practicum.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
-public class StatServer {
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigServerApp {
     public static void main(String[] args) {
-        SpringApplication.run(StatServer.class, args);
+        SpringApplication.run(ConfigServerApp.class, args);
     }
 }
